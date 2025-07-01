49a5db349063d694ccc8099300c9aae19dc1b6e3
sarita595k <sarita595k@gmail.com>
2025-07-01 19:11:35 +0530

    small change

--- src/Components/SideHeader.jsx
+++ src/Components/SideHeader.jsx
@@ -17,8 +17,8 @@ height:100%;
 export const SideHeader = (props) => {
     return (
         <SideHeaderDiv>
-            <LinkIs to={props.article.url}><SideHeaderImg src={props.article.urlToImage ? props.article.urlToImage : "https://tse1.mm.bing.net/th?id=OIP.G18_CzK0wvLvX151D7AiPQHaEe&pid=Api&P=0&h=180"} alt={props.article.title} />
-                <h4>{props.article.title}</h4></LinkIs>
+            <SideHeaderImg src={props.article.urlToImage ? props.article.urlToImage : "https://tse1.mm.bing.net/th?id=OIP.G18_CzK0wvLvX151D7AiPQHaEe&pid=Api&P=0&h=180"} alt={props.article.title} />
+            <LinkIs to={props.article.url}> <h4>{props.article.title}</h4></LinkIs>
         </SideHeaderDiv>
     )
 }
