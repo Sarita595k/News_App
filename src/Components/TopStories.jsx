7bd4cd9cae9ffabe778d6fcfd44dfbab5e71e0be
sarita595k <sarita595k@gmail.com>
2025-07-02 19:54:21 +0530

    small change

--- src/Components/TopStories.jsx
+++ src/Components/TopStories.jsx
@@ -23,7 +23,7 @@ export const TopStories = (props) => {
     return (
         <TopStoriesDiv>
             <TopStoriesImg src={props.article.urlToImage ? props.article.urlToImage : "https://tse1.mm.bing.net/th?id=OIP.G18_CzK0wvLvX151D7AiPQHaEe&pid=Api&P=0&h=180"} />
-            <div>
+            <div className="topStoriesDiv">
                 <TopStoriesTitle>{props.article?.title.slice(0, 70)}...</TopStoriesTitle>
                 <p>{props.article.description?.slice(0, 80)}...</p>
                 <Link to={props.article.url}>Read more</Link>
