c37a799fa6a2f5f8e646fff3363d07119425cd67
sarita595k <sarita595k@gmail.com>
2025-07-03 18:07:18 +0530

    crickte api fetched

---
+++ src/Components/fetchCricketMatch.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export const fetchCricketMatch = () => {
+    const [match, setMatch] = useState([])
+
+    const fetchMatchUrl = () => {
+
+    }
+    return (
+        <h1>Cricket Match</h1>
+    )
+}
